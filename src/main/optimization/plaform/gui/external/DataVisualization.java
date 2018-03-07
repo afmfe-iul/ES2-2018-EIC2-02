fcc1546db26605cafb2761c543772b823795faca
afmfe-iul <andrefm.freire@gmail.com>
2018-03-07 00:26:58 +0000

    Adds more tests and comments to the DataVisualization class

--- src/main/optimization/plaform/gui/external/DataVisualization.java
+++ src/main/optimization/plaform/gui/external/DataVisualization.java
@@ -1,11 +1,20 @@
 package main.optimization.plaform.gui.external;
 
 import java.util.List;
+import javafx.embed.swing.JFXPanel;
 
-public class DataVisualization {
+public class DataVisualization  extends JFXPanel{
+	private static final long serialVersionUID = 1L;
 
 	public DataVisualization(List<String> filePaths) {
-		// TODO Auto-generated constructor stub
+		// TODO generate data.tsv files based on the .rs an .rf files in the filePaths
+		// the .tsv formats should be: Decision Variable Name (column 1), Algorithm 1 (column 2), Algorithm 2, etc..
+		// and Optimiation Criterium number (column 1), Algorithm 1 (column 2), Algorithm 2, etc..
 	}
 
+	public boolean run() {
+		// TODO If there was a problem generating the .tsv files return false, otherwise
+		// start a new webView, set it as this component scene, load a html file to the webView and return true
+		return false;
+	}
 }
--- src/tests/optimization/plaform/gui/external/DataVisualizationTest.java
+++ src/tests/optimization/plaform/gui/external/DataVisualizationTest.java
@@ -5,17 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JComponent;
 import org.junit.Test;
-import javafx.embed.swing.JFXPanel;
 import main.optimization.plaform.gui.external.DataVisualization;
 
-public class DataVisualizationTest extends JFXPanel{
-	private static final long serialVersionUID = 1L;
+public class DataVisualizationTest {
 
 	@Test
 	public void constructorMustReturnAnObjectThatInheritsFromJComponent(){
 		List<String> filePaths = new ArrayList<String>();
 		DataVisualization instance = new DataVisualization(filePaths);
 		assertTrue("DataVisualization intances should extend the JComponent class.",
-				instance.getClass().getSuperclass().equals(JComponent.class));
+				JComponent.class.isAssignableFrom(instance.getClass()));
+	}
+	
+	@Test
+	public void runShouldReturnFalseIfTheFilePathsAreInvalid(){
+		List<String> filePaths = new ArrayList<String>();
+		DataVisualization instance = new DataVisualization(filePaths);
+		assertFalse("DataVisualization run method must return false if there is a problem with the file paths.",
+				instance.run());
 	}
 }
