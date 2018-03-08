8a93f12abaa84f2a2fb488bfd79a85bfea78ce30
“Tiago <tmrfo1@iscte-iul.pt>
2018-03-08 16:57:22 +0000

    Adds unstaged changes

---
+++ src/main/optimization/plaform/jMetal/problems/IntegerDoubleProblem.java
@@ -0,0 +1,27 @@
+package main.optimization.plaform.jMetal.problems;
+
+import java.util.List;
+
+import org.uma.jmetal.problem.impl.AbstractIntegerDoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.solution.Solution;
+
+public class IntegerDoubleProblem<S> extends AbstractIntegerDoubleProblem<S> {
+
+	public IntegerDoubleProblem(List<String> decisionVariables, List<String> jarPaths) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void evaluate(S solution) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public S createSolution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/optimization/plaform/jMetal/problems/IntegerPermutationProblem.java
@@ -0,0 +1,26 @@
+package main.optimization.plaform.jMetal.problems;
+
+import java.util.List;
+
+import org.uma.jmetal.problem.impl.AbstractIntegerPermutationProblem;
+import org.uma.jmetal.solution.PermutationSolution;
+
+public class IntegerPermutationProblem extends AbstractIntegerPermutationProblem {
+
+	public IntegerPermutationProblem(List<String> decisionVariables, List<String> jarPaths) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getPermutationLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void evaluate(PermutationSolution<Integer> solution) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
