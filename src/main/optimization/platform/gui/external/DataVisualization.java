08c9232622daedda0e167c90cb2478ec7488bd8f
afmfe-iul <andrefm.freire@gmail.com>
2018-04-19 20:05:45 +0100

    Adds visualizations for the optimization process results and compares them to the solution known by the user

--- src/main/optimization/platform/gui/external/DataVisualization.java
+++ src/main/optimization/platform/gui/external/DataVisualization.java
@@ -9,6 +9,8 @@ import java.net.CookieManager;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import javax.swing.JFrame;
@@ -26,51 +28,67 @@ public class DataVisualization  extends JFXPanel{
 	private boolean dataFileBuiltSuccessfuly = false;
 	private static int VERSION = 0;
 	
-	// TODO atm this constructor only allows to build from .rs files (decision variables weights)
-	// it must be changed in the future to allow to build .rf files (optimization variables, ex: FP/FN)
-	public DataVisualization(List<String> algorithmsNames, List<String> filePaths, List<String> decisionVariables) {
-		if(algorithmsNames.size() != filePaths.size() || algorithmsNames.size() == 0 || filePaths.size() == 0){
+	public DataVisualization(List<String> algorithmsNames, List<String> rsFilePaths, 
+			List<String> rfFilePaths, List<String> decisionVariables, Integer knownSolution) {
+		
+		if(algorithmsNames.size() != rsFilePaths.size() || algorithmsNames.size() != rfFilePaths.size() ||
+				algorithmsNames.size() == 0 || rsFilePaths.size() == 0 || rfFilePaths.size() == 0){
 			dataFileBuiltSuccessfuly = false;
 		}else{
 			VERSION++;
-			String firstLine = "variable";
-			String[] lines = new String[decisionVariables.size()];
-			for(int i = 0; i < lines.length; i++){
-				lines[i] = decisionVariables.get(i);
-			}
+			String rsFirstLine = "variable";
+			String[] rsLines = new String[decisionVariables.size()];
 			
-			for(int i = 0; i < filePaths.size(); i++){
-		        Scanner sc;
+			String rfFirstLine = "User_Solution";
+			String rfSecondLine = knownSolution == null ? "-1" : String.valueOf(knownSolution);
+			for(int i = 0; i < rsLines.length; i++){
+				rsLines[i] = decisionVariables.get(i);
+			}
+
+			for(int i = 0; i < rsFilePaths.size(); i++){
+		        Scanner rsScanner;
+		        Scanner rfScanner;
 				try {
-					sc = new Scanner(new File(filePaths.get(i)));
+					rsScanner = new Scanner(new File(rsFilePaths.get(i)));
+					rfScanner = new Scanner(new File(rfFilePaths.get(i)));
+					
 					int differentRunsOfTheSameAlgorithm = 0;
-			        while(sc.hasNextLine()){
-			        	firstLine += "\t" + algorithmsNames.get(i) + "(" + differentRunsOfTheSameAlgorithm + ")";
-			            String[] weights = sc.nextLine().split(" ");
+			        while(rsScanner.hasNextLine()){
+			        	// logic to read .rs file
+			        	String thisRunName = algorithmsNames.get(i) + "(" + differentRunsOfTheSameAlgorithm + ")";
+			        	rsFirstLine += "\t" + thisRunName;
+			            String[] weights = rsScanner.nextLine().split(" ");
 			            if(weights.length != decisionVariables.size()){
 			            	dataFileBuiltSuccessfuly = false;
-			            	sc.close();
+			            	rsScanner.close();
 			            	return;
 			            }
+			            
 			            for(int j = 0; j < weights.length; j++){
-			            	lines[j] += "\t" + weights[j]; 
+			            	rsLines[j] += "\t" + weights[j]; 
 			            }
 			            differentRunsOfTheSameAlgorithm++;
-			        } 
-			        sc.close();
+			        
+			            // logic to read .rf file
+			            rfFirstLine += "\t" + thisRunName;
+			            rfSecondLine += ("\t" + calculateRfFileResult(rfScanner.nextLine().split(" ")));
+			        }
+			        rsScanner.close();
+			        rfScanner.close();
 				} catch (FileNotFoundException e) {
 					dataFileBuiltSuccessfuly = false;
 					e.printStackTrace();
 					return;
 				}
 			}
-			writeTemporaryDataFile(firstLine, lines);
+			writeVisualizationFiles(rsFirstLine, rsLines, rfFirstLine, rfSecondLine);
 		}
 	}
 
 	public boolean run() {
 		if(dataFileBuiltSuccessfuly){
-			File f = new File("visualizations/temp" + VERSION + "/graphics.html");
+			String tempDir = CURR_DIR + "visualizations/temp" + VERSION;
+			File f = new File(tempDir + "/graphics.html");
 			try {
 				URL url = f.toURI().toURL();
 				Platform.runLater(() -> {
@@ -87,7 +105,15 @@ public class DataVisualization  extends JFXPanel{
 		return dataFileBuiltSuccessfuly;
 	}
 	
-	private void writeTemporaryDataFile(String firstLine, String[] lines){
+	private String calculateRfFileResult(String[] rfFileLine) {
+		int result = 0;
+		for(int i = 0; i < rfFileLine.length; i++) {
+			result += Double.valueOf(rfFileLine[i]);
+		}
+		return String.valueOf(result);
+	}
+	
+	private void writeVisualizationFiles(String rsFirstLine, String[] rsLines, String rfFirstLine, String rfSecondLine){
 		try {
 			deleteDirectory(new File(CURR_DIR + "visualizations"));
 			String tempDir = CURR_DIR + "visualizations/temp" + VERSION;
@@ -96,9 +122,9 @@ public class DataVisualization  extends JFXPanel{
 			
 			// Write the data file in tsv format (tab separated values)
 			PrintWriter writer = new PrintWriter(tempDir + "/data.tsv");
-			writer.println(firstLine);
-			for(int i = 0; i < lines.length; i++){
-				writer.println(lines[i]);
+			writer.println(rsFirstLine);
+			for(int i = 0; i < rsLines.length; i++){
+				writer.println(rsLines[i]);
 			}
 			writer.close();
 			
@@ -106,12 +132,46 @@ public class DataVisualization  extends JFXPanel{
 			// and use them to loop over map values.
 			writer = new PrintWriter(tempDir + "/auxiliar.tsv");
 			String header = new String();
-			for(int i = 0; i < firstLine.split("\t").length -1; i++){
+			for(int i = 0; i < rsFirstLine.split("\t").length -1; i++){
 				header += "\t" + "algorithm" + i;
 			}
 			writer.println(header.trim());
-			writer.println(firstLine.substring("variable".length()).trim());
+			writer.println(rsFirstLine.substring("variable".length()).trim());
 			writer.close();
+			
+			// Write an auxiliar file to be used by D3.js, where the rf file results are stored
+			writer = new PrintWriter(tempDir + "/auxRf.tsv");
+			
+			// orders the results before saving to a file
+			String[] algoName = rfFirstLine.split("\t");
+			int[] algoResults = Arrays.asList(rfSecondLine.split("\t")).stream().mapToInt(Integer::parseInt).toArray();
+			HashMap<Integer, String> resultToAlgoMap = new HashMap<>();
+			for(int i = 0; i < algoName.length; i++) {
+				if(resultToAlgoMap.containsKey(algoResults[i])){
+					resultToAlgoMap.put(algoResults[i], 
+							resultToAlgoMap.get(algoResults[i]) + "\t" + algoName[i]);
+				}else {
+					resultToAlgoMap.put(algoResults[i], algoName[i]);
+				}
+			}
+			
+			Arrays.sort(algoResults);
+			rfFirstLine = new String();
+			rfSecondLine = new String();
+			for(int i = 0; i < algoResults.length; i++) {
+				String[] s = resultToAlgoMap.get(algoResults[i]).split("\t");
+				if(s.length > 1) {
+					String[] s2 = resultToAlgoMap.get(algoResults[i]).split("\t", 2);
+					resultToAlgoMap.put(algoResults[i], s2[1]);
+				}
+				rfFirstLine += "\t" + s[0];
+				rfSecondLine += "\t" + algoResults[i];
+			}
+			
+			writer.println(rfFirstLine.trim());
+			writer.println(rfSecondLine.trim());
+			writer.close();
+			
 			dataFileBuiltSuccessfuly = true;
 			Platform.setImplicitExit(false);
 		} catch (IOException e) {
@@ -124,11 +184,15 @@ public class DataVisualization  extends JFXPanel{
 		List<String> algorithmsNames = new ArrayList<String>();
 		algorithmsNames.add("NSGAII");
 		
-		List<String> filePaths = new ArrayList<String>();
-		filePaths.add(CURR_DIR + "experimentsBaseDirectory/testResults/AntiSpamFilterProblem.NSGAII.rs");
+		List<String> rsFilePaths = new ArrayList<String>();
+		rsFilePaths.add(CURR_DIR + "experimentsBaseDirectory/AntiSpamFilterProblem/DoubleProblem.NSGAII.rs");
+		
+		List<String> rfFilePaths = new ArrayList<String>();
+		rfFilePaths.add(CURR_DIR + "experimentsBaseDirectory/AntiSpamFilterProblem/DoubleProblem.NSGAII.rf");
+		
 		
 		List<String> decisionVariables = new ArrayList<String>();
-		File file = new File(CURR_DIR + "experimentsBaseDirectory/testResults/rules.cf");
+		File file = new File(CURR_DIR + "experimentsBaseDirectory/AntiSpamFilterProblem/rules.cf");
 		
 		try {
 	        Scanner sc = new Scanner(file);
@@ -141,7 +205,7 @@ public class DataVisualization  extends JFXPanel{
 	    	e.printStackTrace();
 	    }
 		
-		DataVisualization dv = new DataVisualization(algorithmsNames, filePaths, decisionVariables);
+		DataVisualization dv = new DataVisualization(algorithmsNames, rsFilePaths, rfFilePaths, decisionVariables, null);
 		JFrame jFrame = new JFrame("Main");
 		jFrame.setSize(1300, 700);
 		jFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
