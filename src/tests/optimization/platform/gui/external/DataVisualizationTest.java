e10b76de9cb0dfcf9926311a97da483c18070c50
afmfe-iul <andrefm.freire@gmail.com>
2018-04-23 23:09:50 +0100

    Fixes DataVisualizationTest class

--- src/tests/optimization/platform/gui/external/DataVisualizationTest.java
+++ src/tests/optimization/platform/gui/external/DataVisualizationTest.java
@@ -13,9 +13,12 @@ public class DataVisualizationTest {
 	@Test
 	public void constructorMustReturnAnObjectThatInheritsFromJComponent(){
 		List<String> algorithmsNames = new ArrayList<String>();
-		List<String> filePaths = new ArrayList<String>();
+		List<String> rsFilePaths = new ArrayList<String>();
+		List<String> rfFilePaths = new ArrayList<String>();
 		List<String> decisionVariables = new ArrayList<String>();
-		DataVisualization instance = new DataVisualization(algorithmsNames, filePaths, decisionVariables);
+		Integer knownSolution = null;
+		DataVisualization instance = new DataVisualization(algorithmsNames, rsFilePaths,
+				rfFilePaths, decisionVariables, knownSolution);
 		assertTrue("DataVisualization intances should extend the JComponent class.",
 				JComponent.class.isAssignableFrom(instance.getClass()));
 	}
@@ -23,9 +26,12 @@ public class DataVisualizationTest {
 	@Test
 	public void runShouldReturnFalseIfTheFilePathsAreInvalid(){
 		List<String> algorithmsNames = new ArrayList<String>();
-		List<String> filePaths = new ArrayList<String>();
+		List<String> rsFilePaths = new ArrayList<String>();
+		List<String> rfFilePaths = new ArrayList<String>();
 		List<String> decisionVariables = new ArrayList<String>();
-		DataVisualization instance = new DataVisualization(algorithmsNames, filePaths, decisionVariables);
+		Integer knownSolution = null;
+		DataVisualization instance = new DataVisualization(algorithmsNames, rsFilePaths,
+				rfFilePaths, decisionVariables, knownSolution);
 		assertFalse("DataVisualization run method must return false if there is a problem with the file paths.",
 				instance.run());
 	}
