fa749931ab01c428c11664865ca7765676ad9e64
Hugo Alexandre <habar@iscte-iul.pt>
2018-05-16 20:04:06 +0100

    Adds all Builders for Double and Integer problems

--- src/main/optimization/platform/jMetal/OptimizationProcess.java
+++ src/main/optimization/platform/jMetal/OptimizationProcess.java
@@ -6,26 +6,26 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
 import main.optimization.platform.gui.LayoutProblem;
 import main.optimization.platform.gui.TableRowCriteria;
 import main.optimization.platform.gui.TableRowVariable;
 import main.optimization.platform.utils.Builders;
 
 public class OptimizationProcess {
-		public static enum DATA_TYPES {
-			BINARY ("Binary"),
-			INTEGER ("Integer"),
-			DOUBLE ("Double");
-			
-			String type;
-			private DATA_TYPES(String type) {
-				this.type = type;
-			}
-			
-			public String toString() {
-				return type;
-			}
+	public static enum DATA_TYPES {
+		BINARY("Binary"), INTEGER("Integer"), DOUBLE("Double");
+
+		String type;
+
+		private DATA_TYPES(String type) {
+			this.type = type;
 		}
+
+		public String toString() {
+			return type;
+		}
+	}
 
 	/**
 	 * Goes to the zip folder on the dependencies, scans the entire folder for all
@@ -40,7 +40,7 @@ public class OptimizationProcess {
 		zip = new ZipInputStream(this.getClass().getResourceAsStream("/jmetal-algorithm-5.5.1-sources.jar"));
 
 		ArrayList<String> totalAlgo = new ArrayList<String>();
-		getGenericAlgo(totalAlgo);
+		getGenericAlgo(totalAlgo, dataType);
 		if (dataType.equals("Double") || dataType.equals("IntegerDouble")) {
 			for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
 				if (!entry.isDirectory() && entry.getName().endsWith(".java")
@@ -55,7 +55,7 @@ public class OptimizationProcess {
 					while (sc.hasNextLine()) {
 						String line = sc.nextLine();
 						if (line.contains("public class") && !line.startsWith(" ") && !line.contains("CellDE ")
-								&& line.contains("Double")) {
+								&& !line.contains("DMOPSO") && !line.contains("SMPSO") && line.contains("Double")) {
 							String[] array = className.split("\\.");
 							totalAlgo.add(array[array.length - 2]);
 						}
@@ -80,6 +80,7 @@ public class OptimizationProcess {
 					while (sc.hasNextLine()) {
 						String line = sc.nextLine();
 						if (line.contains("public class") && !line.startsWith(" ") && !line.contains("CellDE ")
+								&& !line.contains("DMOPSO") && !line.contains("SMPSO") && !line.contains("RNSGAII")
 								&& line.contains("Binary")) {
 							String[] array = className.split("\\.");
 							totalAlgo.add(array[array.length - 2]);
@@ -95,52 +96,49 @@ public class OptimizationProcess {
 		return totalAlgo;
 	}
 
-	
 	public boolean run(LayoutProblem currentProblem) {
 		List<TableRowVariable> rows = currentProblem.getListVariable();
 		List<String> decisionVariables = new ArrayList<>();
 		List<TableRowCriteria> jarRows = currentProblem.getListCriteria();
 		List<String> jarPaths = new ArrayList<>();
-		for(int i = 0; i < jarRows.size(); i++) {
+		for (int i = 0; i < jarRows.size(); i++) {
 			jarPaths.add(jarRows.get(i).getPath());
 		}
-		
+
 		// Build a Double Problem
 		if (currentProblem.getType().equals("Double")) {
 			List<Double> lowerBounds = new ArrayList<>();
 			List<Double> upperBounds = new ArrayList<>();
-			for(int i = 0; i < rows.size(); i++) {
+			for (int i = 0; i < rows.size(); i++) {
 				decisionVariables.add(rows.get(i).getRule());
 				lowerBounds.add(Double.parseDouble(rows.get(i).getMinimo()));
 				upperBounds.add(Double.parseDouble(rows.get(i).getMaximo()));
 			}
-			return Builders.DoubleBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(), decisionVariables, 
-					lowerBounds, upperBounds, jarPaths);
-		
-		// Build a Integer Problem
-		}else if(currentProblem.getType().equals("Integer")) {
+			return Builders.DoubleBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(),
+					decisionVariables, lowerBounds, upperBounds, jarPaths);
+
+			// Build a Integer Problem
+		} else if (currentProblem.getType().equals("Integer")) {
 			List<Integer> lowerBounds = new ArrayList<>();
 			List<Integer> upperBounds = new ArrayList<>();
-			for(int i = 0; i < rows.size(); i++) {
+			for (int i = 0; i < rows.size(); i++) {
 				decisionVariables.add(rows.get(i).getRule());
 				lowerBounds.add(Integer.valueOf(rows.get(i).getMinimo()));
 				upperBounds.add(Integer.valueOf(rows.get(i).getMaximo()));
 			}
-			return Builders.IntegerBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(), decisionVariables,
-					lowerBounds, upperBounds, jarPaths);
-		
-		// Build a Binary Problem
-		}else if(currentProblem.getType().equals("Binary")) {
-			return Builders.BinaryBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(), decisionVariables,
-					jarPaths, currentProblem.getBitsPerVariable());
+			return Builders.IntegerBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(),
+					decisionVariables, lowerBounds, upperBounds, jarPaths);
+
+			// Build a Binary Problem
+		} else if (currentProblem.getType().equals("Binary")) {
+			return Builders.BinaryBuilder(currentProblem.getProblemTitle(), currentProblem.getListAlgorithms(),
+					decisionVariables, jarPaths, currentProblem.getBitsPerVariable());
 		}
 		return false;
 	}
-	
 
-	
 	@SuppressWarnings("resource")
-	private void getGenericAlgo(ArrayList<String> list) throws IOException {
+	private void getGenericAlgo(ArrayList<String> list, String dataType) throws IOException {
 
 		ArrayList<String> classNames = new ArrayList<String>();
 		ZipInputStream zip;
@@ -153,7 +151,9 @@ public class OptimizationProcess {
 					&& !entry.getName().contains("MOMBI") && !entry.getName().contains("util")
 					&& !entry.getName().contains("Builder") && !entry.getName().contains("Measures")
 					&& !entry.getName().contains("45") && !entry.getName().contains("Steady")
-					&& !entry.getName().contains("Stopping")) {
+					&& !entry.getName().contains("Stopping") && !entry.getName().contains("DMOPSO")
+					&& !entry.getName().contains("SMPSO") && !entry.getName().contains("RNSGAII")
+					&& !entry.getName().contains("WASFGA")) {
 				String className = entry.getName().replace('/', '.'); // including ".class"
 				classNames.add(className.substring(0, className.length() - ".class".length() + 1));
 
@@ -161,7 +161,7 @@ public class OptimizationProcess {
 				while (sc.hasNextLine()) {
 					String line = sc.nextLine();
 					if (line.contains("public class") && !line.startsWith(" ") && !line.contains("CellDE ")
-							&& !line.contains("Double") && !line.contains("Binary")) {
+							&& !line.contains("Double") && !line.contains("Binary") && (dataType.equals("Integer") && !line.contains("IBEA"))) {
 						String[] array = className.split("\\.");
 						list.add(array[array.length - 2]);
 					}
@@ -169,4 +169,12 @@ public class OptimizationProcess {
 			}
 		}
 	}
+	public static void main(String[] args) throws Exception {
+		
+		OptimizationProcess op = new OptimizationProcess();
+		for(String s : op.getAlgorithmsFor("Integer")) {
+			System.out.println(s);
+		}
+	}
+	
 }
